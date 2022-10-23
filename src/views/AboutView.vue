6011c37be8048f3856918435f13d48fec62fdc19
ya9 <devyassine47@gmail.com>
2022-10-23 10:44:31 +0100

    Starter template

---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <router-link to="/">To Home</router-link>
+
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello MyEbag</h1>
+    <router-link to="/about">To About</router-link>
+  </div>
+</template>
+
+<script>
+// Components
+
+export default {
+  name: 'HomeView',
+
+};
+</script>
